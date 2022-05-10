2ea0d4730b32bd5ad9ea06fc42f4e61d0ad974f3
Martin Ratinaud <martinratinaud@gmail.com>
2022-05-10 16:20:46 +0400

    Add 3 endpoints with different imports

--- pages/api/hello-cjs.ts
+++ pages/api/hello-cjs.ts
@@ -5,18 +5,18 @@ type Data = {
   name: string
 }
 // @ts-ignore
-import fetcher from 'open-terms-archive/src/other.js';
+import fetcherCjs from 'open-terms-archive/src/other.cjs.js';
 
 console.log(''); //eslint-disable-line
 console.log('╔════START══fetcher══════════════════════════════════════════════════'); //eslint-disable-line
 // console.log(fetcher); //eslint-disable-line
 // console.log(fetcherEs); //eslint-disable-line
-console.log(fetcher); //eslint-disable-line
+console.log(fetcherCjs); //eslint-disable-line
 console.log('╚════END════fetcher══════════════════════════════════════════════════'); //eslint-disable-line
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'standard' })
+  res.status(200).json({ name: 'Cjs' })
 }
